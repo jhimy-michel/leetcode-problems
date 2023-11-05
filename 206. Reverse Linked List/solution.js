e48651f6f4e6c11a9585787e6769837daa38eddc
JMichel <jhimy0riv@gmail.com>
2023-11-05 20:06:07 +0100

    First problem

---
+++ 206. Reverse Linked List/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function reverseList(head) {
+  let cur = head;
+  let prev = null;
+  let next;
+
+  while (cur !== null) {
+    next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+  }
+  return prev;
+}
